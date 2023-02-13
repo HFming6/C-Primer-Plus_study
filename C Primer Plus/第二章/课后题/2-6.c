ba48e897596a565757db21cee8c69c819d593921
HFming6 <125169110+HFming6@users.noreply.github.com>
2023-02-13 15:21:18 +0800

    Create 2-6.c

---
+++ C Primer Plus/第二章/课后题/2-6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+	int toes = 10;
+	
+	printf("toes = %d\n",toes);
+	printf("toes*2 = %d\n",2*toes);
+	printf("toes^2 = %d\n",toes*toes);
+	
+	return 0;
+}
