6d32b5e3e5551a7b265920734af02863d53121e9
HFming6 <125169110+HFming6@users.noreply.github.com>
2023-02-13 15:21:28 +0800

    Create 2-8.c

---
+++ C Primer Plus/第二章/课后题/2-8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void one_three();
+void two();
+
+int main(void) 
+{
+	printf("starting now:\n");
+	one_three();
+	printf("done!\n");
+	
+	return 0;
+}
+
+void one_three(){
+	printf("one\n");
+	two();
+	printf("three\n");
+}
+
+void two(){
+	printf("two\n");
+}
+
+	
+
