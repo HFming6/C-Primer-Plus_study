e95e2b1344c34455dd745dc3427e4e80de0d4024
HFming6 <125169110+HFming6@users.noreply.github.com>
2023-02-13 15:17:55 +0800

    Create escape.c

---
+++ C Primer Plus/第三章/源代码/escape.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+//使用转义序列(系统不同结果不一定)
+int main(void) 
+{
+	float salary;
+	
+	printf("\aEnter your desired monthly salary:");
+	printf(" $_______\b\b\b\b\b\b\b");
+	scanf("%f",&salary);
+	printf("\n\t$%.2f a month is $%.2f a year.",salary,salary*12.0);
+	printf("\rGee!\n");
+	
+	return 0;
+}
